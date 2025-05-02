dbd2ec238cddbea3cbf216220adaa267eef3bdf4
govind-kumawat2002 <kumawatritik54@gmail.com>
2025-05-02 12:21:35 +0530

    python intro and data types

---
+++ python/basic.py
@@ -0,0 +1,131 @@
+# print("hello world")
+# print("hello world")
+# print("hello world")    
+
+# name = "ritik"
+# print(name)
+# %%%% = 2541616
+# 434 = "jai"
+# Name  = "jai"
+# print(Name)
+# number  = 1000000
+# print(number)
+# num = 10.25852585
+# print(num)
+
+# %%
+# print(5 + 5 * 10)
+# %%
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>....string >>>>>>>>>>>>>>>>>>>>>>>>>
+# name = "ritik"  # 01234
+# print(name[0:2])
+# print(name[4])
+# print(type(name))
+# num = 100.2
+# print(type(num))
+# var = True
+# print(type(var))
+
+# print(len(name))
+# name = "ritik"  # as a string
+# name_upper = "RITIK" # as a string
+# print(name.upper()) # upper case
+# print(name_upper.lower()) # lower case
+# print(name.swapcase())
+# print(name.title())
+
+# print(name.replace("ritik", "jai"))
+
+# name = "ritik"
+# last_name = "kmt"
+
+# print(name + " " +  last_name)
+# output = ritik kmt    ,ritikkmt
+# print(name.find("k"))
+# print(name.capitalize())
+
+
+# my name is ritk
+# print("My name is ", name, "and my last name is ", last_name) 
+
+
+# print(3 * name)
+
+# print(f"my name is  {name}  and my last name is  {last_name}")
+
+
+
+# print(name.replace("i", "j"))
+# print(name.replace("i", "j", 1))
+
+
+
+# text =  " hello world "
+
+# # print(text.replace("world", "python"))
+# print(text.index("world"))
+
+
+
+
+
+#### list 
+
+
+# my_list = [1,2,3,4,5,5,5,5,54,5,5,5,55]
+# print(type(my_list))
+
+
+# my_list1 = [1,"ritik",3,4.2,True,5,5,5,54,5,5,5,55]
+# print(type(my_list1))
+# print(my_list1)
+
+# print(my_list)
+# print(my_list[2])
+
+
+# my_list = [1,2,3,4,5,5,5,5,54,5,5,5,55]
+# print(my_list[-1])
+# print(my_list[-2])
+# print(my_list[-3])
+# print(my_list[-4])
+# print(my_list[-5])
+
+# print(my_list[1])
+# print(my_list[2])
+# print(my_list[3])
+# print(my_list[4])
+# print(my_list[5])
+
+
+# my_list = [1,2,3,4,5,5,5,5,54,5,5,5,55]
+
+
+# print(my_list[1:5])
+# print(my_list[:5])
+# print(my_list[1:])
+# print(my_list[:])
+
+
+# print(len(my_list))
+
+
+# my_list.append(100)
+# print(my_list)
+
+# my_list.insert(1,100)
+# print(my_list)
+# my_list.remove(5)
+# print(my_list)
+# my_list.pop()
+# print(my_list)
+# my_list.index(5)
+# print(my_list)
+
+# my_num = [5,4,3,2,1]
+# my_num.sort()
+# my_num.clear()
+# print(my_num)
