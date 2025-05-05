83b9a46758b65a24e9f7c7d8eb6c6bd8d1fef219
govind-kumawat2002 <kumawatritik54@gmail.com>
2025-05-05 12:21:44 +0530

    data type and conditional statement

--- python/basic.py
+++ python/basic.py
@@ -129,3 +129,223 @@
 # my_num.sort()
 # my_num.clear()
 # print(my_num)
+
+
+
+
+# >>>tuple  and dict , set
+
+
+
+
+
+
+# operations on tuple 
+
+# tpl = (1,2,3,4,5)
+# print("tpl:-",tpl)
+
+# print("type of tpl:-",type(tpl))
+# print("len  of tpl:-",len(tpl))
+
+# print("0 position ka element :-",tpl[0])
+# print("1 position ka element :-",tpl[1])
+# print("2 position ka element :-",tpl[2])
+# print("3 position ka element :-",tpl[3])
+# print("4 position ka element :-",tpl[4])
+# print("0 position ka element :-",tpl[5])
+# print("0 position ka element :-",tpl[6])
+
+
+
+# tpl = (1,2,3,4,5)
+
+# 
+# print("0 to 2  position ka element :-",tpl[0:2])
+# print("1 position ka element :-",tpl[::-1])  
+
+
+
+# print("1 position ka element :-",tpl[1:-1])  # anshul   2 , 3 , 4   
+
+# tpl = (1,2,3,4,5)
+
+# print(2 in tpl)
+# print(10  in tpl)
+
+# min 
+# max
+# sum
+
+
+# print("max of tpl:-",max(tpl))
+# print("min of tpl:-",min(tpl))
+# print("sum of tpl:-",sum(tpl))
+
+
+
+# tpl1 = 1,2,3,54,5,4,4
+# print(type(tpl1))
+# print(tpl1)
+
+
+
+# tuple unpaking
+# kapil = 1,2,3 
+
+# a,b,c = kapil
+# print("a:-",a)
+# print("b:-",b)
+# print("c:-",c)
+
+# data list dict tuple
+
+
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>..dict 
+
+dct = {
+    "name" : "ritik",
+    "last_name" : "kmt",
+    "age" : 21
+}
+# print("dict type:-",type(dct))
+# print("dct :-",dct)
+
+# print(dct["name"])
+
+# print("name :-",dct["name"])#  >>>>> ritik 
+# print("last_name :-",dct["last_name"])# >>>> kmt 
+# print("age :-",dct["age"]) #>>>>> 21 
+
+
+
+# print("dct keys:-",dct.keys())
+# print("dct values:-",dct.values())
+# print("dct items:-",dct.items())
+
+
+
+
+dct = {
+    "name" : "ritik",
+    "last_name" : "kmt",
+    "age" : 21
+} 
+
+# item adding in dct 
+
+# dct['Gender'] = "Male"   #  # Add new key-value pair
+# print(dct)
+
+# updating dct 
+
+# dct['age'] = 22
+# print(dct)
+
+
+# Deleting items 
+# del dct['last_name']
+# print(dct) 
+# dct.pop("last_name")
+# print(dct)
+
+# dct.clear()
+# print(dct)
+# print('name' in dct)
+# print('ritik' in dct)
+
+
+# dct = {
+#     "name" : "ritik",
+#     "last_name" : "kmt",
+#     "age" : 21
+# } 
+
+# copy 
+
+# dct1 = dct.copy()
+# print(dct1)
+
+# copy_dct=dct.copy()
+# print("copy dict ",copy_dct)
+# print("original dict ",dct)
+
+
+
+#  Merging Dictionaries
+
+
+# dct1 = {
+#     "vikram_name" : "vikram",
+  
+#     "vikram_age" : 21
+# } 
+
+# dct2 = {
+#     "kapil_name" : "kapil",
+#     "last_name" : "kumar",
+#     "kapil_age" : 21
+# } 
+
+# dct1.update(dct2)
+# print(dct1)
+
+
+# key = ["a","b","c"]
+# default_dict=dict.fromkeys(key, 0)
+
+# print(default_dict) ### >>>>>>>>>>>>>.{'a': 0, 'b': 0, 'c': 0}
+
+
+
+# >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Set 
+
+# my_set = {1,2,3,4,5,"ritik"}
+# print(type(my_set))
+# # print(my_set)
+
+# my_set.add(100)
+# my_set.add("kapil")
+# my_set.add("vijay")
+
+# print(my_set)
+
+# my_set.remove("ritik")
+# print("remove element:-",my_set)
+# my_set.discard(5)
+# print("discard element:-",my_set)
+
+# my_set.pop()
+# print("pop element:-",my_set)
+# my_set.clear()
+# print("clear element:-",my_set)
+
+
+# """""""""""""""operator """
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
